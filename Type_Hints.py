fced801f1c8a95f7c7adcc574670d9dc23b7fdbd
Neobraingit <Neobraingit@github.com>
2022-11-27 16:41:36 +0100

    Type hints

---
+++ Type_Hints.py
@@ -0,0 +1,7 @@
+my_string_varibale = 'My string variable'
+print (type(my_string_varibale))
+
+my_string_varibale = 5
+print (type(my_string_varibale))
+
+my_type_variable = 'My string typed variable'
